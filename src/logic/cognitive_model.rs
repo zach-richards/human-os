4db2b6248019bce56b33d7425a17e97ca22706fc
Zach <zacharyryanrichards@gmail.com>
2026-02-18 10:26:06 -0800

    Made a formula to calculate focus

--- src/logic/cognitive_model.rs
+++ src/logic/cognitive_model.rs
@@ -1,15 +1,42 @@
 // cognitive_model.rs
 
+fn calc_score() {
+    FocusScore =
+        (NormKPS * 0.40) + // keystrokes a min (capped at 5 KPS)
+        (SwitchScore * 0.30) + // amount of switching
+        (IdleScore * 0.20) + // idling
+        (BackspaceScore * 0.10) // amount of backspacing
+}
+
+/*
+
+KPS = keystrokes per second (over 60 seconds)
+NormKPS = min(KPS / 5.0, 1.0)
+
+SwitchRate = switches per minute
+NormSwitch = min(SwitchRate / 3.0, 1.0)
+SwitchScore = 1 - NormSwitch
+
+IdleRatio = idle_seconds / 60
+IdleScore = 1 - IdleRatio
+
+BackspaceRatio = backspaces / total_keystrokes
+NormBackspace = min(BackspaceRatio / 0.25, 1.0)
+BackspaceScore = 1 - NormBackspace
+
+*/
+
+
 enum FocusState {
     Flow,
     Focus,
     Neutral,
     Distracted,
-    Fatigue/Strain,
+    Fatigued,
 }
 
 impl FocusState {
-    pub fn from_score(score: f32) -> Self {
+    fn from_score(score: f32) -> Self {
         use FocusState::*;
 
         match score {
@@ -17,10 +44,23 @@ impl FocusState {
             0.60..0.79 => Focus,
             0.40..0.59 => Neutral,
             0.20..0.39 => Distracted,
-            _ => Fatigue/Strain,
+            _ => Fatigued,
         }
     }
 }
 
-struct CognitiveModel {
-}V
+pub struct CognitiveModel {
+    score: f32,
+    state: FocusState,
+}
+
+impl CognitiveModel {
+    pub fn new() -> Self {
+        score: 0.50,
+        state: FocusState::from_score(score);
+    }
+
+    pub update() {
+        
+    }
+}
