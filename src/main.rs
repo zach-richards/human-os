fa96c7a78badae0e16021a35f886ac537adc44df
Zach <zacharyryanrichards@gmail.com>
2026-02-18 22:42:09 -0800

    Working on thread issues

--- src/main.rs
+++ src/main.rs
@@ -3,84 +3,87 @@
 mod sys;
 
 use std::time::{ Duration, Instant };
-use std::sync::{ Arc, Mutex };
+use std::sync::{ mpsc, Arc, Mutex };
 use std::thread;
 
 use rdev::{ listen, Event, EventType, ListenError, Key };
 use once_cell::sync::Lazy;
 
 use crate::sys::mouse;
-use crate::sys::system;
+use crate::sys::system::SystemInfo;
 use crate::sys::window;
 use crate::sys::keyboard;
 
-// create global variable to share across the system
-static SYSTEM_INFO: Lazy<Arc<Mutex<sys::system::SystemInfo>>> =
-    Lazy::new(|| Arc::new(Mutex::new(system::SystemInfo::new())));
+// Global Arc<Mutex>
+static SYSTEM_INFO: Lazy<Arc<Mutex<SystemInfo>>> = Lazy::new(|| Arc::new(Mutex::new(SystemInfo::new())));
+
+// Global sender for rdev callback
+static GLOBAL_TX: Lazy<Mutex<Option<mpsc::Sender<Event>>>> = Lazy::new(|| Mutex::new(None));
 
 static THROTTLE: Duration = Duration::from_millis(50);
 
-fn main() -> Result<(), ListenError> {
+fn event_callback(event: Event) {
+    // must be a function pointer, cannot capture variables
+    if let Some(tx) = &*GLOBAL_TX.lock().unwrap() {
+        tx.send(event).unwrap();
+    }
+}
+
+fn main() {
     #[cfg(debug_assertions)]
     println!("  DEBUG LOG");
     println!("--------------");
 
-    thread::spawn(move || {
+    let (tx, rx) = mpsc::channel();
 
-        listen(move |event: Event| {
-            println!("Hello thread");
-        
-            // track keyboard, mouse, and mouse buttons in seperate thread
-            if let Ok(mut sys_info) = SYSTEM_INFO.lock() {
-                println!("Hello hello");
-                match event.event_type {
-                    EventType::KeyPress(Key::Backspace) => {
-                        keyboard::handle_backspace(&mut sys_info);
-                    }
+    // store sender globally
+    *GLOBAL_TX.lock().unwrap() = Some(tx);
 
-                    EventType::KeyPress(_) => {
-                        keyboard::handle_key_press(&mut *sys_info);
-                    }
-
-                    EventType::ButtonPress(_) => {
-                        mouse::handle_button_press(&mut *sys_info);
-                    }
-
-                    EventType::MouseMove {..} => {
-                        if sys_info
-                            .last_mouse_move
-                            .map_or(true, |t| Instant::now().duration_since(t) >= THROTTLE)
-                        {
-                            mouse::handle_mouse_move(&mut *sys_info);
-                        }
-                    }
-
-                    EventType::Wheel {..} => {
-                        if sys_info
-                            .last_wheel_scroll
-                            .map_or(true, |t| Instant::now().duration_since(t) >= THROTTLE)
-                        {
-                            mouse::handle_wheel_scroll(&mut *sys_info);
-                        }
-                        
-                    }
-
-                    _ => { /* ignore */ }
-                } 
-            
-            }
-        }).unwrap();
-        
+    // listener thread
+    thread::spawn(|| {
+        listen(event_callback).unwrap();
     });
 
+    // worker thread
+    let sys_info_clone = SYSTEM_INFO.clone();
+    thread::spawn(move || {
+        while let Ok(event) = rx.recv() {
+            let mut sys_info = sys_info_clone.lock().unwrap();
+
+            match event.event_type {
+                EventType::KeyPress(Key::Backspace) => {
+                    keyboard::handle_backspace(&mut sys_info);
+                    println!("Backspace pressed!");
+                }
+                EventType::KeyPress(_) => keyboard::handle_key_press(&mut sys_info),
+                EventType::ButtonPress(_) => mouse::handle_button_press(&mut sys_info),
+                EventType::MouseMove { .. } => {
+                    if sys_info.last_mouse_move.map_or(true, |t| Instant::now().duration_since(t) >= THROTTLE)
+                    {
+                        mouse::handle_mouse_move(&mut sys_info);
+                    }
+                }
+                EventType::Wheel { .. } => {
+                    if sys_info.last_wheel_scroll.map_or(true, |t| Instant::now().duration_since(t) >= THROTTLE)
+                    {
+                        mouse::handle_wheel_scroll(&mut sys_info);
+                    }
+                }
+                _ => {}
+            }
+        }
+    });
+/*
     // track window switches in different thread
     thread::spawn(move || {
 
         if let Ok(mut sys_info) = SYSTEM_INFO.lock() {
             window::track_window_switches(&mut *sys_info).unwrap();
         }
-        
-    });
 
-    loop {}
+    });
+*/
+    loop {
+        thread::sleep(Duration::from_secs(1));
+    }
 }
